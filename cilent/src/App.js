437fa889b554226a104e8827498ee22701cd0267
Kieran Rooney <kieranrooney@Kierans-MacBook-Pro.local>
2022-06-29 15:57:52 +0100

    client

--- cilent/src/App.js
+++
@@ -1,8 +0,0 @@
-import React, {useEffect} from 'react';
-
-function App() {
-  useEffect(() => {
-    fetch('http://localhost:9000/')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  });
